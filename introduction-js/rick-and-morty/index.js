89db935fe2965059d1f1aa43c8246f9abd99cdd6
debnix <daquipe5@gmail.com>
2022-09-05 10:18:16 -0500

    introduction-js/rick-and-morty: add function mostrar_personajes

--- introduction-js/rick-and-morty/index.js
+++ introduction-js/rick-and-morty/index.js
@@ -5,13 +5,36 @@ async function obtener_personajes (url_api) {
     method: 'GET' //, 'POST', 'PUT', 'DELETE'
   })
   const data = await resp.json()
-  console.log(data.results)
+  //console.log(data.results)
+  return data.results
+}
+
+function mostrar_personajes (personajes) {
+  let cards = ''
+  for (let i = 0; i < personajes.length; i++) {
+    // template string
+    cards += `
+      <article class="card">
+				<img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+				<div class="body-card">
+						<h2>Rick Sanchez</h2>
+						<span>Alive - Human</span>
+						<span>Gender: Male</span>
+						<span>Location: Earth</span>
+						<span>Origin: Earth</span>
+				</div>
+			</article>
+    `
+  }
+  // Adicionar las tarjetas al section
+  document.getElementById('section-cards').innerHTML = cards
 }
 
 
-function main () {
+async function main () {
   const url = "https://rickandmortyapi.com/api/character"
-  obtener_personajes(url)
+  const personajes = await obtener_personajes(url)
+  mostrar_personajes(personajes)
 }
 
 main()
