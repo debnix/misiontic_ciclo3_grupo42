a355d3bbbe170ea4b3b43c1206ce4705bd0ecbe4
debnix <daquipe5@gmail.com>
2022-09-06 10:31:00 -0500

    users_project: add script.sql

---
+++ users_project/script.sql
@@ -0,0 +1,19 @@
+
+# Consultar base de datos
+SHOW DATABASES;
+
+# Crear base de datos
+CREATE DATABASE utp_grupo_42;
+
+# Consultar tablas de la BD
+SHOW TABLES;
+
+# Crear tabla
+CREATE TABLE users(
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    email VARCHAR(250) NOT NULL,
+    fecha_nacimiento DATE NOT NULL,
+    foto TEXT NOT NULL
+);
