39f06f24ed025b0870c12531dd76ade7582421dd
debnix <daquipe5@gmail.com>
2022-09-12 10:14:59 -0500

    introduction-hibernate: call buscarPersona into App.java

--- introduction-hibernate/src/main/java/com/hibernate/introduction/App.java
+++ introduction-hibernate/src/main/java/com/hibernate/introduction/App.java
@@ -11,6 +11,6 @@ public class App {
     public static void main(String[] args) {
         PersonaController controller = new PersonaController();
         PersonaView view = new PersonaView(controller);
-        view.actualizarPersona();
+        view.buscarPersona();
     }
 }
--- introduction-hibernate/src/main/java/com/hibernate/introduction/view/PersonaView.java
+++ introduction-hibernate/src/main/java/com/hibernate/introduction/view/PersonaView.java
@@ -1,7 +1,6 @@
 package com.hibernate.introduction.view;
 
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 import javax.swing.JOptionPane;
