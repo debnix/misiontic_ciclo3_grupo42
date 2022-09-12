15b92526cdd2e6a14ae0de599302dd6bceeaa943
debnix <daquipe5@gmail.com>
2022-09-12 09:16:42 -0500

    introduction-hibernate: add method obtenerPersonas into PersonaController

--- introduction-hibernate/src/main/java/com/hibernate/introduction/controller/PersonaController.java
+++ introduction-hibernate/src/main/java/com/hibernate/introduction/controller/PersonaController.java
@@ -1,6 +1,8 @@
 package com.hibernate.introduction.controller;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -21,19 +23,36 @@ public class PersonaController {
         .buildSessionFactory();
   }
 
+  private Session crearSesion() {
+    Session session = factory.openSession();
+    session.beginTransaction();
+    return session;
+  }
+
   /** ACCIONES */
   public boolean crearPersona(String nombre, String apellido, String email, Date fecha_naci, String foto) {
     boolean create = false;
-    Session session = factory.openSession();
-    session.beginTransaction();
+    Session session = crearSesion();
     try {
       Persona persona = new Persona(nombre, apellido, email, fecha_naci, foto);
       session.persist(persona);
       session.getTransaction().commit();
       create = true;
+      session.close();
     } catch (Exception e) {
       e.printStackTrace();
     }
     return create;
+  }
+
+  public List<Persona> obtenerPersonas() {
+    List<Persona> personas = new ArrayList<>();
+    Session session = crearSesion();
+    try {
+      personas = session.createQuery("from Persona", Persona.class).list();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return personas;
   }
 }
