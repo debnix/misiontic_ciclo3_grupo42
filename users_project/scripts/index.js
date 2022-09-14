9e75ae7c0811d0a52593f02302f26b643ac6dcf3
debnix <daquipe5@gmail.com>
2022-09-14 10:21:09 -0500

    users_project: add show_users method

--- users_project/scripts/index.js
+++ users_project/scripts/index.js
@@ -8,10 +8,33 @@ async function get_users (url) {
   return users;
 }
 
+function show_users (users) {
+  // Referenciar tabla
+  const table = document.getElementById("table")
+  let tbody = "<tbody>"
+  // Iterar users
+  for (let i = 0; i < users.length; i++) {
+    const arrayDate = users[i].fecha_nacimiento.split("T")
+    tbody += `
+      <tr>
+        <td>
+          <img src="${users[i].foto}" />
+        </td>
+        <td>${users[i].nombre}</td>
+        <td>${users[i].apellido}</td>
+        <td>${users[i].email}</td>
+        <td>${arrayDate[0]}</td>
+      </tr>
+    `
+  }
+  tbody += "</tbody>"
+  table.innerHTML += tbody
+}
+
 async function main () {
   const url = "http://localhost:8080/personas"
   const users = await get_users(url)
-  console.log(users)
+  show_users(users)
 }
 
 main()
